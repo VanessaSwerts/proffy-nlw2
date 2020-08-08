7340b0e9bfba9fe7f13327e736146fa2583c0df2
VanessaSwerts <vanessaswerts@gec.inatel.br>
2020-08-08 01:49:46 -0300

    last class

--- src/server.js
+++ src/server.js
@@ -1,72 +1,8 @@
-//Dados
-const proffys = [
-    {
-        name: "Diego Fernandes",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-        whatsapp: "99987658",
-        bio: "Entusiasta das melhores tecnologias de química avançada.Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões",
-        subject: "Química",
-        cost: "20",
-        weekday: [0],
-        tim_from: [720],
-        time_to: [1220]
-    },    
-]
-
-const subjects = [
-    "Artes",
-    "Biologia",
-    "Ciências",
-    "Educação Física",
-    "Física",
-    "Geografia",
-    "História",
-    "Matemática",
-    "Português",
-    "Química",
-]
-
-const weekdays = [
-    "Domingo",
-    "Segunda-feira",
-    "Terça-feira",
-    "Quarta-feira",
-    "Quinta-feira",
-    "Sexta-feira",
-    "Sábado",
-]
-
-//Funcionalidades
-function getSubject(subjectNumber) {
-    const position = +subjectNumber - 1
-    return subjects[position]
-}
-
-function pageLanding(req, res) {
-    return res.render("index.html")
-}
-
-function pageStudy(req, res) {
-    const filters = req.query
-    return res.render("study.html", { proffys, filters, subjects, weekdays })
-}
-
-function pageGiveClasses(req, res) {
-    const data = req.query
-
-    const isNotEmpty = Object.keys(data).length > 0
-    if (isNotEmpty) {
-        data.subject = getSubject(data.subject)
-        proffys.push(data)
-        return res.redirect("/study")
-    }
-
-    return res.render("give-classes.html", { subjects, weekdays })
-}
-
 //Servidor
 const express = require('express')
 const server = express()
+
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require('./pages')
 
 const nunjucks = require('nunjucks')
 nunjucks.configure('src/views', {
@@ -75,8 +11,10 @@ nunjucks.configure('src/views', {
 })
 
 server
+    .use(express.urlencoded({ extended: true }))
     .use(express.static("public"))
     .get("/", pageLanding)
     .get("/study", pageStudy)
     .get("/give-classes", pageGiveClasses)
+    .post("/save-classes", saveClasses)
     .listen(5500)
